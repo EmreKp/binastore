94e038085db55a7439f5dcd3476578b2cee1627f
binoaga <binaribey@gmail.com>
2015-07-12 02:01:22 +0300

    Added image

--- urunler.php
+++ urunler.php
@@ -15,18 +15,33 @@ $idsec="SELECT * FROM kullanici WHERE nick='".$nick."'";
 $id=mysqli_fetch_assoc(mysqli_query($bag,$idsec));
 $isim=$_POST["isim"]; $fiyat=$_POST["fiyat"];
 $detay=$_POST["detay"]; $satici=$id["userid"];
-if ($isim!="") {
- $ekle="INSERT INTO urunler (isim, fiyat, detay, satici) VALUES ('".$isim."','".$fiyat."','".$detay."','".$satici."')";
+$resim=$_FILES["resim"]["tmp_name"];
+if ($resim!="") { 
+  $gonder=2;
+  $turu=pathinfo($resim,PATHINFO_EXTENSION);
+  $resmad=md5(basename($_FILES["resim"]["name"])).$turu;
+  $dosya="resim/".$resmad;
+  if ($turu!="jpg" || $turu!="gif" || $turu!="png" || $turu!="jpeg" || $turu!="bmp") { 
+  echo "<div class=\"hata\">Resminizin türü geçerli değil.</div>"; $gonder=0; }
+  else if ($_FILES["resim"]["size"]>2210000) {
+  echo "<div class=\"hata\">Resminiz 2 MB'tan büyük olamaz.</div>"; $gonder=0; }
+  else try { move_uploaded_file($resim,$dosya); 
+  echo "<div class=\"bravo\">Resminiz eklendi.</div>"; }
+  catch(Exception $e) { echo "<div class=\"hata\">Hata oluştu: ".$e->getMessage(); $gonder=0; }
+}
+if ($isim!="" && $gonder!=0) {
+ $ekle="INSERT INTO urunler (isim, fiyat, detay, satici, resimadi) VALUES ('".$isim."','".$fiyat."','".$detay."','".$satici."','".$resmad."')";
  if (mysqli_query($bag,$ekle)) echo "<div class=\"bravo\">Ürününüz eklendi.</div>";
  else echo "<div class=\"hata\">Hata oluştu: ".mysqli_error();
 } 
 echo "<h1>Ürün Paneli</h1>
 <h2>Ürün Ekleme</h2>
 <p>Ürününüzün bilgilerini forma girin ve gönderin.</p>
-<form method=\"post\"><div id=\"urunform\">
+<form method=\"post\" enctype=\"multipart/form-data\"><div id=\"urunform\">
 <p><label>Ürünün Adı:</label> <input type=\"text\" name=\"isim\" required></p>
 <p><label>Fiyat (TL):</label> <input type=\"text\" name=\"fiyat\" required></p>
 <p><label>Açıklama:</label> <textarea name=\"detay\" placeholder=\"Ürününüzle ilgili açıklamalar...\" required></textarea></p>
+<p><label>Resim: </label><input type=\"file\" name=\"resim\" accept=\"image/*\"></p>
 <p><input type=\"submit\" value=\"Ekle\"></p>
 </div></form>"; }
 else { echo "\n<form action=\"index.php\" method=\"post\">
